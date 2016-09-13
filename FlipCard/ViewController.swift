1cf0fbf7d4f5479aaa65619e48ed92d28af773b2
Alexis Padula <apadula2@carpenter-08.elon.edu>
2016-09-13 15:53:04 -0400

    storyboard works, flip not

--- FlipCard/ViewController.swift
+++ FlipCard/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var cardImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func flipTapRecognition(_ sender: UITapGestureRecognizer) {
+        if  cardImageView.image == #imageLiteral(resourceName: "front") {
+            cardImageView.image=#imageLiteral(resourceName: "back")
+        }
+        else{
+            cardImageView.image = #imageLiteral(resourceName: "front")
+        }
+        
     }
+    
 
 
 }
